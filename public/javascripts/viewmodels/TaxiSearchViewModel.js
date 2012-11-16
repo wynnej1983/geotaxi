bb3050567379da9311f63548b52472740a86df7a
Jonathan Wynne <wynnej1983@gmail.com>
2012-11-17 05:06:30 +0800

    added knockout, sammyjs libs and loading current position via google maps using viewmodels for pages

--- public/javascripts/viewmodels/TaxiSearchViewModel.js
+++ public/javascripts/viewmodels/TaxiSearchViewModel.js
@@ -4,7 +4,7 @@ define(["gmaps"], function (gmaps){
       var map_el = document.getElementById("map");
       var map = new gmaps.Map(map_el, {
         center: new gmaps.LatLng(position.coords.latitude, position.coords.longitude),
-        zoom: 15,
+        zoom: 20,
         mapTypeId: gmaps.MapTypeId.ROADMAP
       });
     }, function(error) {
